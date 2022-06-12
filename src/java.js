68bd6690d25770cd2d95918eeac8cdc2c3be77a1
Brcnerden <brcnerden@gmail.com>
2022-06-12 18:17:20 +0300

    deneme1

--- src/java.js
+++ src/java.js
@@ -1,1 +1,21 @@
+var dogum = prompt ("dogum yılınızı giriniz" );
+var yıl = 2022;
+var yas = yıl-dogum;
 
+if( yas<18) {
+    console.log("ehliyet alamazsınız");
+
+    console.log("ehliyet alabilmeniz için "+(18-yas)+"sene beklemelisiniz");
+}
+
+
+   
+
+
+
+else{
+    console.log("yanlış bir değer");
+
+}
+
+console.log("hoş geldiniz")
